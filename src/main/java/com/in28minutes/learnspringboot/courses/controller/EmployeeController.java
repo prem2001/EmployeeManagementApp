1280ba0f4c04cc032bd0909f1496ce863195414a
prem <premnath101070@gmail.com>
2022-06-01 12:54:28 +0530

    remove space commit

--- src/main/java/com/in28minutes/learnspringboot/courses/controller/EmployeeController.java
+++ src/main/java/com/in28minutes/learnspringboot/courses/controller/EmployeeController.java
@@ -41,7 +41,6 @@ public class EmployeeController {
 			response.setMessage("Register Successfully");
             response.setIsSuccess(true);
 			registerRepository.save(model);
-			
 		} else {
 		
 			response.setMessage("Already exist");
